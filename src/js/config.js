6324fec53bf17ea5e8a27d5352820de07bcb9f14
Andranik Sarkisyan <andrewsarkisyan@gmail.com>
2023-04-16 17:47:18 +0300

    add test token for open public beta

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+export const apiUrl = 'https://api.themoviedb.org'
+export const apiKey = 'dcaf7f5ea224596464b7714bac28142f'
+export const lang = 'en'
+
+export const translate = [
+    {lang: 'en', data: ['Top movies', 'Top TVs', 'Top Mixed', 'Top Persons']},
+    {lang: 'ru', data: ['Фильмы', 'Сериалы', 'Все вместе', 'Люди']}
+]
