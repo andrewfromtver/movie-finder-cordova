4b4f5f56d613b5fd472b15ef640b34a307b604a8
Andranik Sarkisyan <andrewsarkisyan@gmail.com>
2023-06-24 19:35:06 +0000

    Update file render.js

--- src/js/render.js
+++ src/js/render.js
@@ -1421,19 +1421,8 @@ function PrintDiv(div)
 {
     html2canvas((div), {
         onrendered: function(canvas) {
-            var myImage = canvas.toDataURL();
-            downloadURI(myImage, "MaSimulation.png");
+            var img = canvas.toDataURL("image/png");
+            document.write('<img src="'+img+'"/>');
       }
     });
-}
-
-function downloadURI(uri, name) {
-    var link = document.createElement("a");
-
-    link.download = name;
-    link.href = uri;
-    document.body.appendChild(link);
-    link.click();   
-    //after creating link you should delete dynamic link
-    //clearDynamicLink(link); 
 }
