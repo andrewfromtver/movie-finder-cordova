6ecd5d407bd5ff2f9667ed89a6f4ae4bcd8a4323
Andranik Sarkisyan <andrewsarkisyan@gmail.com>
2023-06-24 21:06:58 +0000

    Update file render.js

--- src/js/render.js
+++ src/js/render.js
@@ -164,8 +164,8 @@ export const renderItem = (type, id) => {
                               <div  style="color: #dc3545;" class="pb-2">${
                                 data.release_date || data.last_air_date || ""
                               }</div>
-                              <table class="mb-4" style="width: 100%;" id="seasonsTable">${episodeCountInner}</table>
-                              <div class="d-flex" id="itemControllButtons">
+                              <table style="width: 100%;" id="seasonsTable">${episodeCountInner}</table>
+                              <div class="mt-4 d-flex" id="itemControllButtons">
                                   <button id="showRecommendations" class="m-2 p-2 btn btn-secondary flex-shrink-0" type="button">
                                       ${showButtonText}
                                   </button>
