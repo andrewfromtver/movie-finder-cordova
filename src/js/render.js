769aa588da2ecebf2727a4eb02fee926d4b0c006
Andranik Sarkisyan <andrewsarkisyan@gmail.com>
2023-10-17 03:29:35 +0000

    Update file render.js

--- src/js/render.js
+++ src/js/render.js
@@ -1027,7 +1027,7 @@ export const renderWebTorPlayer = () => {
                 </div>
                 <div class="mt-4 row gx-4 gx-lg-5 align-items-center">
                     <div class="col-md-12">
-                        <iframe class="shadow-sm movieFrame" height="0px" style="background: #000000;" id="output" sandbox="allow-same-origin"></iframe>
+                        <iframe class="shadow-sm movieFrame" height="0px" style="background: #000000;" id="output" sandbox="allow-scripts allow-same-origin"></iframe>
                     </div>
                 </div>
             </div>
